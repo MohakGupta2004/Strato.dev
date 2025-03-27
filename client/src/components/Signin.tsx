d9f53197b9e77ef8b00053a71e4bbda93a63f7f3
GTAJIT <mukhujeegtajit@gmail.com>
2025-03-28 02:43:22 +0530

    test?

--- client/src/components/Signin.tsx
+++ client/src/components/Signin.tsx
@@ -67,49 +67,52 @@ function Signin() {
 
     return (
         <>
-            <div>
-                <div>
-                    <div className="">
-                        <div>
-                            <div className="">
+            <div className="flex justify-center items-center">
+                <div className="border p-5 rounded-md bg-gray-100">
+                    <div className="flex flex-col items-center justify-center gap-5">
+                        <div className="flex flex-col items-center">
+                            <div className="border-2 rounded-xl p-2">
                                 <img src={logo} alt="" />
                             </div>
                             <p>{LoginChange} to AI-AGENT</p>
                         </div>
-                        <div>
-                            <form onSubmit={isLogin ? handleSubmitSignin : handleSubmitSignup}>
-                                <div>
-                                    <label className="">Email</label>
-                                    <input onChange={handleInputChange}
-                                        value={personDetails.email}
-                                        type="email"
-                                        name="email"
-                                        id="email"
-                                        placeholder="example@example.com"
-                                        className="" />
-                                </div>
-                                <div>
-                                    <label className="">Password</label>
-                                    <input
-                                        type={isChecked ? "text" : "password"}
-                                        placeholder="Your password"
-                                        name="password"
-                                        id="password"
-                                        onChange={handleInputChange}
-                                        value={personDetails.password}
-                                        className=""
-                                    />
-                                    <div>
-                                        <input
-                                            type="checkbox"
-                                            checked={isChecked}
-                                            onChange={handleChange}
-                                        />
-                                        <label>Show Password</label>
+                        <div className="flex flex-col" >
+                            <form className="flex flex-col gap-10" onSubmit={isLogin ? handleSubmitSignin : handleSubmitSignup}>
+                                <div className="flex flex-col gap-4">
+                                    <div className="flex flex-col">
+                                        <label className="">Email</label>
+                                        <input onChange={handleInputChange}
+                                            value={personDetails.email}
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            placeholder="example@example.com"
+                                            className="border-2 rounded-md p-3" />
+                                    </div>
+                                    <div className="flex flex-col" >
+                                        <label className="">Password</label>
+                                        <div className="flex items-center border-2 rounded-md">
+                                            <input
+                                                type={isChecked ? "text" : "password"}
+                                                placeholder="Your password"
+                                                name="password"
+                                                id="password"
+                                                onChange={handleInputChange}
+                                                value={personDetails.password}
+                                                className="flex flex-col p-3"
+                                            />
+                                            <span className="flex flex-col" >
+                                                <input
+                                                    type="checkbox"
+                                                    checked={isChecked}
+                                                    onChange={handleChange}
+                                                />
+                                            </span>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="">
-                                    <button type="submit" className="" >{LoginChange}</button>
+                                <div className="flex flex-col">
+                                    <button type="submit" className="text-white bg-blue-600 dark:text-black dark:bg-blue-500 border-none rounded-sm" >{LoginChange}</button>
                                     {error && <p className="text-center text-red-700 p-4">{error}</p>}
                                 </div>
                             </form>
